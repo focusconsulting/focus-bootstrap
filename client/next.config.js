b896a04a539c07c2029bbed3cdad3361b8c4f052
Iverson Diles <ivcotad@gmail.com>
2024-05-08 14:48:58 -0700

    config for spa only (for now)

--- client/next.config.js
+++ client/next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
   reactStrictMode: true,
   // Output only the necessary files for a deployment, excluding irrelevant node_modules
   // https://nextjs.org/docs/app/api-reference/next-config-js/output
-  output: "standalone",
+  output: "export",
   sassOptions: appSassOptions,
   // Continue to support older browsers (ES5)
   transpilePackages: [
